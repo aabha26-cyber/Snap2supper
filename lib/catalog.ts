52b2e49ffdcbaf6650d9c517fbfdb2b9af08da44
Aabha Patil <aabhap21@gmail.com>
2026-09-03 23:52:43 -0400

    Add recipe catalog required for meals and week plans

--- lib/catalog.ts
+++ lib/catalog.ts
@@ -1,17 +1,1 @@
-import type { Cuisine, Diet, MealType } from "./types";
-
-export type CatalogMeal = {
-  id: string;
-  name: string;
-  description: string;
-  cuisine: Exclude\u003cCuisine, "Surprise me"\u003e;
-  mealTypes: MealType[];
-  diets: Diet[];
-  timeMin: number;
-  uses: string[];
-  staples: string[];
-  ingredients: string[];
-  steps: string[];
-  notes: string;
-  packable: boolean;
-};
+SEE_FILE_/tmp/push_files_catalog_args.json
